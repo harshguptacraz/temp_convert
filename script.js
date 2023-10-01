ec97166f4cd3caeab70305c16d5d579d3923704b
harshguptacraz <146618687+harshguptacraz@users.noreply.github.com>
2023-10-01 12:37:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+function convertToCelsius() {
+    var fahrenheit = parseFloat(document.getElementById("fahrenheit").value);
+    var celsius = (fahrenheit - 32) * 5/9;
+    document.getElementById("celsius").value = celsius.toFixed(2);
+}
+
+function convertToFahrenheit() {
+    var celsius = parseFloat(document.getElementById("celsius").value);
+    var fahrenheit = (celsius * 9/5) + 32;
+    document.getElementById("fahrenheit").value = fahrenheit.toFixed(2);
+}
